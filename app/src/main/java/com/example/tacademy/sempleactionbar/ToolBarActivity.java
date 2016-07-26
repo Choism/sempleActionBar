2d684f596875f3dc9b91a8fc9496836ea16a9a7b
Seok Min Choi <pcgo118@gmail.com>
2016-07-26 16:39:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/sempleactionbar/ToolBarActivity.java
@@ -0,0 +1,18 @@
+package com.example.tacademy.sempleactionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ToolBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+}
